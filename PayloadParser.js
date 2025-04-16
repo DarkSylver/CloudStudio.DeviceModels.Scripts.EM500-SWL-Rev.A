0a95169918c00b7c29b54be0d100c240e45a566d
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-04-16 17:52:47 -0300

    Initial commit
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

---
+++ PayloadParser.js
@@ -0,0 +1,142 @@
+function parseUplink(device, payload) {
+
+    var payloadb = payload.asBytes();
+    var decoded = Decoder(payloadb, payload.port)
+    env.log(decoded);
+
+    // Store battery
+    if (decoded.battery != null) {
+        var sensor1 = device.endpoints.byAddress("1");
+
+          
+        if (sensor1 != null)
+            sensor1.updateVoltageSensorStatus(decoded.battery);
+
+        if (decoded.lowBattery) {
+            device.updateDeviceBattery({ voltage: decoded.battery, state: batteryState.low });
+        }
+        else {
+            device.updateDeviceBattery({ voltage: decoded.battery });
+        }
+    };
+
+    // Store Distance
+    if (decoded.water_level != null) {
+        var sensor2 = device.endpoints.byAddress("2");
+
+        if (sensor2 != null)
+            sensor2.updateGenericSensorStatus(decoded.water_level);
+    };
+
+}
+
+function buildDownlink(device, endpoint, command, payload) 
+{ 
+	// This function allows you to convert a command from the platform 
+	// into a payload to be sent to the device.
+	// Learn more at https://wiki.cloud.studio/page/200
+
+	// The parameters in this function are:
+	// - device: object representing the device to which the command will
+	//   be sent. 
+	// - endpoint: endpoint object representing the endpoint to which the 
+	//   command will be sent. May be null if the command is to be sent to 
+	//   the device, and not to an individual endpoint within the device.
+	// - command: object containing the command that needs to be sent. More
+	//   information at https://wiki.cloud.studio/page/1195.
+
+	// This example is written assuming a device that contains a single endpoint, 
+	// of type appliance, that can be turned on, off, and toggled. 
+	// It is assumed that a single byte must be sent in the payload, 
+	// which indicates the type of operation.
+
+/*
+	 payload.port = 25; 	 	 // This device receives commands on LoRaWAN port 25 
+	 payload.buildResult = downlinkBuildResult.ok; 
+
+	 switch (command.type) { 
+	 	 case commandType.onOff: 
+	 	 	 switch (command.onOff.type) { 
+	 	 	 	 case onOffCommandType.turnOn: 
+	 	 	 	 	 payload.setAsBytes([30]); 	 	 // Command ID 30 is "turn on" 
+	 	 	 	 	 break; 
+	 	 	 	 case onOffCommandType.turnOff: 
+	 	 	 	 	 payload.setAsBytes([31]); 	 	 // Command ID 31 is "turn off" 
+	 	 	 	 	 break; 
+	 	 	 	 case onOffCommandType.toggle: 
+	 	 	 	 	 payload.setAsBytes([32]); 	 	 // Command ID 32 is "toggle" 
+	 	 	 	 	 break; 
+	 	 	 	 default: 
+	 	 	 	 	 payload.buildResult = downlinkBuildResult.unsupported; 
+	 	 	 	 	 break; 
+	 	 	 } 
+	 	 	 break; 
+	 	 default: 
+	 	 	 payload.buildResult = downlinkBuildResult.unsupported; 
+	 	 	 break; 
+	 }
+*/
+
+}
+
+/**
+ * Payload Decoder for The Things Network
+ *
+ * Copyright 2023 Milesight IoT
+ *
+ * @product EM500-SWL
+ */
+function Decoder(bytes, port) {
+    return milesight(bytes);
+}
+
+function milesight(bytes) {
+    var decoded = {};
+
+    for (var i = 0; i < bytes.length; ) {
+        var channel_id = bytes[i++];
+        var channel_type = bytes[i++];
+        // BATTERY
+        if (channel_id === 0x01 && channel_type === 0x75) {
+            decoded.battery = bytes[i];
+            i += 1;
+        }
+        // WATER LEVEL
+        else if (channel_id === 0x03 && channel_type === 0x77) {
+            decoded.water_level = readUInt16LE(bytes.slice(i, i + 2));
+            i += 2;
+        }
+        // HISTROY DATA
+        else if (channel_id === 0x20 && channel_type === 0xce) {
+            var point = {};
+            point.timestamp = readUInt32LE(bytes.slice(i, i + 4));
+            point.water_level = readUInt16LE(bytes.slice(i + 4, i + 6));
+
+            decoded.history = decoded.history || [];
+            decoded.history.push(point);
+            i += 6;
+        } else {
+            break;
+        }
+    }
+
+    return decoded;
+}
+
+/* ******************************************
+ * bytes to number
+ ********************************************/
+function readUInt16LE(bytes) {
+    var value = (bytes[1] << 8) + bytes[0];
+    return value & 0xffff;
+}
+
+function readInt16LE(bytes) {
+    var ref = readUInt16LE(bytes);
+    return ref > 0x7fff ? ref - 0x10000 : ref;
+}
+
+function readUInt32LE(bytes) {
+    var value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
+    return value & 0xffffffff;
+}
